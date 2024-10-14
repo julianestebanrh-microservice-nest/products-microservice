26b22fa74d4ab1a24efedbb15a0941e2c9b9d11a
Julian Esteban <julianestebanrh.contact@gmail.com>
2024-10-13 20:47:41 -0500

    ✨ New feature: Validate Products
    Description:

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -9,37 +9,33 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  // @Post()
   @MessagePattern({ cmd: 'create' })
   create(@Payload() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
-  // @Get()
-  @MessagePattern({ cmd: 'find-all' })
+  @MessagePattern({ cmd: 'findAll' })
   findAll(@Payload() paginationDto: PaginationDto) {
     return this.productsService.findAll(paginationDto);
   }
 
-  // @Get(':id')
   @MessagePattern({ cmd: 'findOne' })
   findOne(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.findOne(+id);
   }
 
-  // @Patch(':id')
   @MessagePattern({ cmd: 'update' })
-  update(
-    // @Param('id', ParseIntPipe) id: number,
-    // @Body() updateProductDto: UpdateProductDto,
-    @Payload() updateProductDto: UpdateProductDto,
-  ) {
+  update(@Payload() updateProductDto: UpdateProductDto) {
     return this.productsService.update(updateProductDto.id, updateProductDto);
   }
 
-  // @Delete(':id')
   @MessagePattern({ cmd: 'remove' })
   remove(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.remove(id);
+  }
+
+  @MessagePattern({ cmd: 'validateProducts' })
+  validate(@Payload() ids: number[]) {
+    return this.productsService.validate(ids);
   }
 }
