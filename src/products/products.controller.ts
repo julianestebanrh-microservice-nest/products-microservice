71a2ce4b38d037ea29de07820b1b8837b39a0ca1
Julian Esteban <julianestebanrh.contact@gmail.com>
2024-10-14 19:22:38 -0500

    ✨ New feature: Cambiando conexion TCP a NATS
    Description:

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -9,32 +9,32 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @MessagePattern({ cmd: 'create' })
+  @MessagePattern('createProducts')
   create(@Payload() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
-  @MessagePattern({ cmd: 'findAll' })
+  @MessagePattern('findAllProducts')
   findAll(@Payload() paginationDto: PaginationDto) {
     return this.productsService.findAll(paginationDto);
   }
 
-  @MessagePattern({ cmd: 'findOne' })
+  @MessagePattern('findOneProduct')
   findOne(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.findOne(+id);
   }
 
-  @MessagePattern({ cmd: 'update' })
+  @MessagePattern('updateProduct')
   update(@Payload() updateProductDto: UpdateProductDto) {
     return this.productsService.update(updateProductDto.id, updateProductDto);
   }
 
-  @MessagePattern({ cmd: 'remove' })
+  @MessagePattern('removeProduct')
   remove(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.remove(id);
   }
 
-  @MessagePattern({ cmd: 'validateProducts' })
+  @MessagePattern('validateProducts')
   validate(@Payload() ids: number[]) {
     return this.productsService.validate(ids);
   }
