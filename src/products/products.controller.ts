80b9f0049fa9e864dbe122e819953caa16ec6c98
Julian Esteban <julianestebanrh.contact@gmail.com>
2024-11-04 19:18:57 -0500

    💚 Continuous Integration: Dockerfile para produccion & cloudbuild de google cloud
    Description:

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -9,32 +9,32 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @MessagePattern('createProducts')
+  @MessagePattern('product.create')
   create(@Payload() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
-  @MessagePattern('findAllProducts')
+  @MessagePattern('product.find.all')
   findAll(@Payload() paginationDto: PaginationDto) {
     return this.productsService.findAll(paginationDto);
   }
 
-  @MessagePattern('findOneProduct')
+  @MessagePattern('product.find.one')
   findOne(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.findOne(+id);
   }
 
-  @MessagePattern('updateProduct')
+  @MessagePattern('product.update')
   update(@Payload() updateProductDto: UpdateProductDto) {
     return this.productsService.update(updateProductDto.id, updateProductDto);
   }
 
-  @MessagePattern('removeProduct')
+  @MessagePattern('product.remove')
   remove(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.remove(id);
   }
 
-  @MessagePattern('validateProducts')
+  @MessagePattern('product.validate')
   validate(@Payload() ids: number[]) {
     return this.productsService.validate(ids);
   }
